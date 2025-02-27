f6331372ee33225025401f4ce86eb92b75256eca
Anubhav <anubhav.paraashar@gmail.com>
2025-02-27 15:07:40 +0530

    second commit

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Layout.css'
+import Header from '../Header/Header'
 
 const Layout = () => {
   return (
     <div className="dashboard">
       <aside className="sidebar">Sidebar</aside>
-      <header className="header">Header</header>
+      {/* <header className="header"><Header /></header> */}
       <section className="cards">Cards Section</section>
       <section className="charts">Charts Section</section>
       <section className="last-section">Last Section</section>
